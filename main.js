f7710df64453307d3fa0e93bfe914092752fa0f6
Alamito <alamirbobroski12@gmail.com>
2023-03-14 16:28:16 -0300

    feat - adicionei funcao de coletar as fraquezas dos tipos de pokemon

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ const getAttributesPokemon = async (URL) => {
     return await attributesPokemon;
 };
 
-const RangeGetPokemon = async (min = 1, max = 1008) => {
+const rangeGetPokemon = async (min = 1, max = 1008) => {
     for (let i = min; i <= max; i++) {
         const URLIdPokemon = `https://pokeapi.co/api/v2/pokemon/${i}`;
         Pokemon = await getAttributesPokemon(URLIdPokemon);
@@ -24,4 +24,31 @@ const RangeGetPokemon = async (min = 1, max = 1008) => {
     }
 };
 
-RangeGetPokemon(1, 10);
+rangeGetPokemon(1, 10); // test case
+
+getWeaknessesType = async (type) => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
+    const data = await response.data;
+    const typeWeakness = await data.damage_relations.double_damage_from;
+
+    let Weaknesses = [];
+
+    typeWeakness.forEach((type) => {
+        Weaknesses.push(type.name);
+    });
+
+    return Weaknesses;
+};
+
+const typesPokemon = ['bug', 'dark', 'dragon', 'electric', 'fairy', 'fighting', 'fire', 'flying', 'ghost', 'grass', 'ground', 'ice', 'normal', 'poison', 'psychic', 'rock', 'steel', 'water'];
+
+rangeGetTypeWeakness = async (types) => {     
+    for (const type of types) {
+        const Weaknesses = await getWeaknessesType(type); 
+        console.log(type, Weaknesses);
+    }
+}
+
+rangeGetTypeWeakness(typesPokemon); // test case
+
+
