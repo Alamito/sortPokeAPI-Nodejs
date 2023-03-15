b1455642c3144999284321de25f2e6be0471ce85
Alamito <alamirbobroski12@gmail.com>
2023-03-15 20:34:05 -0300

    feat - adicionada funcao que cria um arquivo com um tipo de pokemon e coloca o id do pokemon no seu determinado tipo, tambem foi criada uma funcao que deleta os arquivos antigos

--- main.js
+++ main.js
@@ -1,84 +1,6 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const checkExistFile = (path) => {
-    path = `./${path}.bin`;
-
-    if (fs.existsSync(path)) {
-        return true;
-    } else {
-        return false;
-    }
-};
-
-const deleteFile = (path) => {
-    path = `./${path}.bin`;
-
-    try {
-        fs.unlinkSync(path);
-        console.log('Arquivo deletado com sucesso!');
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-const writeFileAttributesPokemon = (Pokemon) => {
-    try {
-        fs.appendFileSync(
-            './Pokemons.bin',
-            `${Pokemon.id};${Pokemon.name};${Pokemon.type};${Pokemon.xp};${Pokemon.height};${Pokemon.weight}\n`,
-            'utf8',
-        );
-        console.log(`Pokemon ${Pokemon.id} salvo com sucesso!`);
-    } catch (err) {
-        console.error(err);
-    }
-};
-
-const getAttributesPokemon = async (URL) => {
-    const response = await axios.get(URL);
-    const data = response.data;
-
-    const attributesPokemon = {
-        id: data.id,
-        name: data.name,
-        type: data.types[0].type.name,
-        xp: data.base_experience,
-        height: data.height / 10,
-        weight: data.weight / 10,
-    };
-
-    return attributesPokemon;
-};
-
-const rangeGetPokemon = async (min = 1, max = 1008) => {
-    if (checkExistFile('Pokemons'))
-        deleteFile('Pokemons');
-
-    for (let idPokemon = min; idPokemon <= max; idPokemon++) {
-        const URLIdPokemon = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
-        Pokemon = await getAttributesPokemon(URLIdPokemon);
-
-        writeFileAttributesPokemon(Pokemon);
-    }
-};
-
-rangeGetPokemon(1, 10); // test case
-
-const getWeaknessesType = async (type) => {
-    const response = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
-    const data = await response.data;
-    const typeWeakness = await data.damage_relations.double_damage_from;
-
-    let Weaknesses = [];
-
-    typeWeakness.forEach((type) => {
-        Weaknesses.push(type.name);
-    });
-
-    return Weaknesses;
-};
-
 const typesPokemon = [
     'bug',
     'dark',
@@ -99,6 +21,109 @@ const typesPokemon = [
     'steel',
     'water',
 ];
+
+const checkExistFile = (path, directory = '') => {
+    path = `./${directory}/${path}.bin`;
+
+    if (fs.existsSync(path)) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const deleteFile = (fileName, directory = '') => {
+    path = `./${directory}/${fileName}.bin`;
+
+    try {
+        fs.unlinkSync(path);
+        console.log(`Arquivo ${fileName} deletado com sucesso!`);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const deleteOldFiles = (types) => {
+    checkExistFile('Pokemons') && deleteFile('Pokemons');
+    types.forEach((type) => {
+        checkExistFile(type, 'types') && deleteFile(type, 'types');
+    });
+};
+
+deleteOldFiles(typesPokemon);
+
+const writeFileAttributesPokemon = (Pokemon) => {
+    try {
+        fs.appendFileSync(
+            './Pokemons.bin',
+            `${Pokemon.id};${Pokemon.name};${Pokemon.xp};${Pokemon.height};${Pokemon.weight}\n`,
+            'utf8',
+        );
+        console.log(`Pokemon ${Pokemon.id} salvo com sucesso!`);
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const writeIdPokemonPerType = (typePokemon, idPokemon) => { 
+    try {
+        fs.appendFileSync(
+            `./types/${typePokemon}.bin`,
+            `${idPokemon}\n`,
+            'utf8',
+        );
+        console.log(`Pokemon ${idPokemon} salvo no tipo ${typePokemon} com sucesso!`);
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+const getAttributesPokemon = async (URL) => {
+    const response = await axios.get(URL);
+    const data = response.data;
+
+    const attributesPokemon = {
+        id: data.id,
+        name: data.name,
+        type: data.types[0].type.name,
+        xp: data.base_experience,
+        height: data.height / 10,
+        weight: data.weight / 10,
+    };
+
+    return attributesPokemon;
+};
+
+
+
+const rangeGetPokemon = async (min = 1, max = 1008) => {
+    if (checkExistFile('Pokemons'))
+        deleteFile('Pokemons');
+
+    for (let idPokemon = min; idPokemon <= max; idPokemon++) {
+        const URLIdPokemon = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
+        Pokemon = await getAttributesPokemon(URLIdPokemon);
+
+        writeFileAttributesPokemon(Pokemon);
+        writeIdPokemonPerType(Pokemon.type, Pokemon.id);
+    }
+};
+
+rangeGetPokemon(1, 10); // test case
+
+const getWeaknessesType = async (type) => {
+    const response = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);
+    const data = await response.data;
+    const typeWeakness = await data.damage_relations.double_damage_from;
+
+    let Weaknesses = [];
+
+    typeWeakness.forEach((type) => {
+        Weaknesses.push(type.name);
+    });
+
+    return Weaknesses;
+};
 
 const rangeGetTypeWeakness = async (types) => {
     for (const type of types) {
