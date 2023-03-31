4d9cd464348fde7d0b4616ff02daa30ac509e614
Alamito <alamirbobroski12@gmail.com>
2023-03-31 16:13:09 -0300

    feat - add tree trie and serach pokemon by prefix name

--- main.js
+++ main.js
@@ -24,7 +24,103 @@ const typesPokemon = [
     'water',
 ];
 
-const checkExistFolder = (directory) => {   
+class TrieNode {
+    constructor(endOfWord = false) {
+        this.children = new Map();
+        this.endOfWord = endOfWord;
+    }
+
+    add(char, node) {
+        this.children.set(char, node);
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word) {
+        let currentNode = this.root;
+
+        for (let i = 0; i < word.length; i++) {
+            const endOfWord = i == word.length - 1;
+
+            if (currentNode.children.get(word[i]) === undefined) {
+                currentNode.add(word[i], new TrieNode(endOfWord));
+            } else if (endOfWord) {
+                currentNode.children.get(word[i]).endOfWord = true;
+            }
+
+            currentNode = currentNode.children.get(word[i]);
+        }
+    }
+
+    searchCompleteWord(word) {
+        let currentNode = this.root;
+        let endOfWord = false;
+
+        for (let i = 0; i < word.length; i++) {
+            if (currentNode.children.get(word[i]) === undefined) {
+                return false;
+            }
+
+            endOfWord = currentNode.children.get(word[i]).endOfWord;
+            currentNode = currentNode.children.get(word[i]);
+        }
+
+        if (endOfWord) {
+            return word;
+        }
+
+        return false;
+    }
+
+    searchPrefix(prefix) {
+        let currentNode = this.root;
+
+        for (let i = 0; i < prefix.length; i++) {
+            if (currentNode.children.get(prefix[i]) === undefined) {
+                return [];
+            }
+
+            currentNode = currentNode.children.get(prefix[i]);
+        }
+
+        return this.wordsToAutocomplete(prefix, currentNode);
+    }
+
+    wordsToAutocomplete(prefix, node) {
+        const stack = [];
+        const matches = [];
+
+        if (node.endOfWord) {
+            matches.push(prefix);
+        }
+
+        for (let [key, child] of node.children) {
+            stack.push([key, child, []]);
+        }
+
+        while (stack.length > 0) {
+            let [char, node, currentWord] = stack.pop();
+
+            currentWord.push(char);
+
+            if (node.endOfWord) {
+                matches.push(currentWord.join(''));
+            }
+
+            for (let [key, child] of node.children) {
+                stack.push([key, child, [...currentWord]]);
+            }
+        }
+
+        return matches;
+    }
+}
+
+const checkExistFolder = (directory) => {
     if (fs.existsSync(directory)) {
         return true;
     } else {
@@ -32,11 +128,11 @@ const checkExistFolder = (directory) => {
     }
 };
 
-const foldersName = ["types", "weaknesses", "strengths"];
+const foldersName = ['types', 'weaknesses', 'strengths'];
 
-const createFolders = () => {               
+const createFolders = () => {
     try {
-        foldersName.forEach(folderName => {
+        foldersName.forEach((folderName) => {
             const directory = `./${folderName}`;
             if (!checkExistFolder(directory)) {
                 fs.mkdirSync(directory);
@@ -45,9 +141,9 @@ const createFolders = () => {
     } catch (err) {
         console.log(err);
     }
-    
+
     return new Promise((resolve) => resolve());
-}
+};
 
 const checkExistFile = (path, directory = '') => {
     path = `./${directory}/${path}.bin`;
@@ -60,7 +156,7 @@ const checkExistFile = (path, directory = '') => {
 };
 
 const deleteFile = (fileName, directory = '') => {
-    path = `./${directory}/${fileName}.bin`;
+    const path = `./${directory}/${fileName}.bin`;
 
     try {
         fs.unlinkSync(path);
@@ -76,23 +172,30 @@ const deleteOldFiles = (types) => {
         checkExistFile(type, 'types') && deleteFile(type, 'types');
 
         checkExistFile(type, 'weaknesses') && deleteFile(type, 'weaknesses');
-        
+
         checkExistFile(type, 'strengths') && deleteFile(type, 'strengths');
     });
 
-    return new Promise(resolve => resolve());
+    return new Promise((resolve) => resolve());
 };
+
+var trie = new Trie();
 
 const rangeGetPokemon = async (min = 1, max = 1008) => {
     for (let idPokemon = min; idPokemon <= max; idPokemon++) {
         const URLIdPokemon = `https://pokeapi.co/api/v2/pokemon/${idPokemon}`;
         Pokemon = await getAttributesPokemon(URLIdPokemon);
 
+        InsertNamePokemonInTrie(Pokemon.name);
         writeFileAttributesPokemon(Pokemon);
         writeIdPokemonPerType(Pokemon.type1, Pokemon.type2, Pokemon.id);
     }
 
     return new Promise((resolve) => resolve());
+};
+
+const InsertNamePokemonInTrie = (name) => {
+    trie.insert(name);
 };
 
 const getAttributesPokemon = async (URL) => {
@@ -128,13 +231,9 @@ const writeFileAttributesPokemon = (Pokemon) => {
     }
 };
 
-const writeAttributesPokemonInFile = (Pokemon) => { 
-    fs.appendFileSync(
-            './Pokemons.bin',
-            `${Pokemon.id};${Pokemon.name};${Pokemon.xp};${Pokemon.height};${Pokemon.weight}\n`,
-            'utf8',
-    );
-    console.log(`Pokemon ${Pokemon.id} salvo em Pokemons.bin!`)
+const writeAttributesPokemonInFile = (Pokemon) => {
+    fs.appendFileSync('./Pokemons.bin', `${Pokemon.id};${Pokemon.name};${Pokemon.xp};${Pokemon.height};${Pokemon.weight}\n`, 'utf8');
+    console.log(`Pokemon ${Pokemon.id} salvo em Pokemons.bin!`);
 };
 
 const writeIdPokemonPerType = (type1, type2, idPokemon) => {
@@ -146,30 +245,22 @@ const writeIdPokemonPerType = (type1, type2, idPokemon) => {
 };
 
 const writeIdInFile = (type1, type2, idPokemon) => {
-    fs.appendFileSync(
-        `./types/${type1}.bin`,
-        `${idPokemon}\n`,
-        'utf8',
-    );
+    fs.appendFileSync(`./types/${type1}.bin`, `${idPokemon}\n`, 'utf8');
     console.log(`Pokemon ${idPokemon} salvo no tipo ${type1}`);
 
     if (existType(type2)) {
-        fs.appendFileSync(
-            `./types/${type2}.bin`,
-            `${idPokemon}\n`,
-            'utf8',
-        );
+        fs.appendFileSync(`./types/${type2}.bin`, `${idPokemon}\n`, 'utf8');
         console.log(`Pokemon ${idPokemon} salvo no tipo ${type2}`);
     }
-}
+};
 
-const existType = (type) => { 
+const existType = (type) => {
     if (type != null) {
         return true;
     } else {
         return false;
     }
-}
+};
 
 const rangeGetTypeWeakness = async (types) => {
     for (const type of types) {
@@ -205,11 +296,7 @@ const writeFileWeaknessesType = (type, Weaknesses) => {
 
 const writeWeaknessInFile = (type, Weaknesses) => {
     Weaknesses.forEach((Weakness) => {
-        fs.appendFileSync(
-            `./weaknesses/${type}.bin`,
-            `${Weakness}\n`,
-            'utf8',
-        );
+        fs.appendFileSync(`./weaknesses/${type}.bin`, `${Weakness}\n`, 'utf8');
         console.log(`Tipo ${Weakness} salvo no dir weaknesses!`);
     });
 };
@@ -247,22 +334,19 @@ const writeFileStrengthsType = (type, Strengths) => {
 
 const writeStrengthInFile = (type, Strengths) => {
     Strengths.forEach((Strength) => {
-        fs.appendFileSync(
-            `./strengths/${type}.bin`,
-            `${Strength}\n`,
-            'utf8'
-        );
+        fs.appendFileSync(`./strengths/${type}.bin`, `${Strength}\n`, 'utf8');
         console.log(`Tipo ${Strength} salvo no dir strengths!`);
     });
 };
 
-const runTasksSynchronously = async (typesPokemon) => { 
+const runTasksSynchronously = async (typesPokemon) => {
     await deleteOldFiles(typesPokemon);
     await createFolders();
     await rangeGetPokemon(1, 50);
     await rangeGetTypeWeakness(typesPokemon);
     await rangeGetTypeStrength(typesPokemon);
-}
+    searchPokemonsByPrefix('ch');
+};
 
 const runTasksAsynchronously = async (typesPokemon) => {
     await deleteOldFiles(typesPokemon);
@@ -272,6 +356,28 @@ const runTasksAsynchronously = async (typesPokemon) => {
     rangeGetTypeStrength(typesPokemon);
 };
 
-// runTasksSynchronously(typesPokemon);    // tempo de execucao 1'25'' (medido apenas uma vez)
+runTasksSynchronously(typesPokemon); // tempo de execucao 1'25'' (medido apenas uma vez)
 
 // runTasksAsynchronously(typesPokemon);   // tempo de execucao 3'20'' (medido apenas uma vez)
+
+// ----------------- ALGORITMO DA ARVORE TRIE ----------------- //
+
+// const trie = new Trie();
+// trie.insert('abacate');
+// trie.insert('abacaxi');
+// trie.insert('abutre');
+// trie.insert('bola');
+// trie.insert('bolo');
+// trie.insert('bolao');
+
+// const prefix = 'AB';
+
+// const matches = trie.searchPrefix(prefix.toLowerCase());
+
+const searchPokemonsByPrefix = (prefix) => {
+    const matches = trie.searchPrefix(prefix.toLowerCase());
+    matches.forEach((match) => {
+        if (match === prefix) console.log(match);
+        else console.log(`${prefix.toLowerCase()}${match}`);
+    });
+};
